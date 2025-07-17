ba9c3914ae300ed87d17d3bf254be37086310d08
Ijas Iqbal <ijasiqbal4@gmail.com>
2025-07-17 15:59:27 +0530

    domain adde to allowed host

--- habittracker/settings.py
+++ habittracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8x1=yj-*bh&z1^0%lqmq$k3v7g^l9=tcyq6yo%zngtucgeghb=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
